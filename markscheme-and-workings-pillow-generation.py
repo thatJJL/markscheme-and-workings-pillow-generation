63cbf2e67dc8cbbc0f9daf512ac9700b0f114e8c
JJL <94117412+thatJJL@users.noreply.github.com>
2022-05-27 16:04:39 +0100

    i5 Updated create A4 to create page defaulting to A4

--- markscheme-and-workings-pillow-generation.py
+++ markscheme-and-workings-pillow-generation.py
@@ -1,17 +1,19 @@
-#markscheme-and-workings-pillow-generation4
-#Create A4 page
+#markscheme-and-workings-pillow-generation5
+#Upadated create A4 page to create page which then defaults to A4 size at 300 dpi
 
 import private.import_or_install as ioi
 
 from PIL import Image
 
 
-def create_A4_page():
+#def dpi_to_pixels(): #For converting page dimensions to pixels using dpi.
+
+def create_page(height = 2480, width = 3508):
     #https://forums.techguy.org/threads/pixel-size-of-a-single-a4-word-document.447822/
     #"2480 x 3508 pixels would give you the equivalent to an A4 sheet at 300 dots-per-inch. This would match the typical draft output from an inkjet printer."
     #2480x3508 at 300 dpi
 
-    img  = Image.new( mode = "RGB", size = (2480, 3508) )
+    img  = Image.new( mode = "RGB", size = (height, width) )
     
     return img
 
@@ -21,8 +23,11 @@ def main():
     #import_or_install("Pillow") #Works but then the import name is different from the install name
     ioi.import_or_install("Pillow", "PIL")
 
-    page = create_A4_page()
+    page = create_page()
     page.show()
+
+    square = create_page(300, 300)
+    square.show()
 
 
 
