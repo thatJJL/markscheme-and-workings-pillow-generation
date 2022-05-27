0dd25ae6cdf173e53aa0d7f7c5bafad5317aaee0
JJL <94117412+thatJJL@users.noreply.github.com>
2022-05-27 15:59:05 +0100

    i4 Create A4 page function

--- markscheme-and-workings-pillow-generation.py
+++ markscheme-and-workings-pillow-generation.py
@@ -1,18 +1,29 @@
-#markscheme-and-workings-pillow-generation3
-#
+#markscheme-and-workings-pillow-generation4
+#Create A4 page
+
 import private.import_or_install as ioi
 
 from PIL import Image
 
 
 def create_A4_page():
-    print("WIP")
+    #https://forums.techguy.org/threads/pixel-size-of-a-single-a4-word-document.447822/
+    #"2480 x 3508 pixels would give you the equivalent to an A4 sheet at 300 dots-per-inch. This would match the typical draft output from an inkjet printer."
+    #2480x3508 at 300 dpi
+
+    img  = Image.new( mode = "RGB", size = (2480, 3508) )
+    
+    return img
 
 
 def main():
     print("main function")
     #import_or_install("Pillow") #Works but then the import name is different from the install name
     ioi.import_or_install("Pillow", "PIL")
+
+    page = create_A4_page()
+    page.show()
+
 
 
     #For checking if it is installed
