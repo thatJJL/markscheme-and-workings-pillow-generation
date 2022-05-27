d32a6433901025b1c666c2520c5b14a6c07bd288
JJL <94117412+thatJJL@users.noreply.github.com>
2022-05-27 15:23:02 +0100

    i2 Encapsulated import_or_install and importing it

--- markscheme-and-workings-pillow-generation.py
+++ markscheme-and-workings-pillow-generation.py
@@ -1,33 +1,13 @@
-#markscheme-and-workings-pillow-generation.py1
-#Checking import_or_install works or how I get it to work again.
-
-def import_or_install(package): #From my "2021-11-09 Screenshot save\3 timings PRODUCT. WORKING.py"
-    import pip
-    import importlib #https://stackoverflow.com/questions/26743012/python-cant-find-importlib
-    try:
-        return __import__(package)
-    except ImportError:
-        print(f"'{package}' not loaded")
-        #return None
-        #pip.main(['install', package])
-
-        pip.main(['install', '--user', package]) #Does do something. ###
-
-        #import_or_install(package) #Could probably just use the import function too.
-        #^^^ Loops
-
-        #return __import__(package)
-
-
-        #https://stackoverflow.com/questions/9806963/how-to-use-the-import-function-to-import-a-name-from-a-submodule
-        importlib.import_module(package)
+#markscheme-and-workings-pillow-generation2
+#Importing import_or_install function
+import private.import_or_install as ioi
 
 
 
 def main():
     print("main function")
     #import_or_install("Pillow") #Works but then the import name is different from the install name
-    import_or_install("Pillow")
+    ioi.import_or_install("Pillow", "PIL")
 
 
     #For checking if it is installed
